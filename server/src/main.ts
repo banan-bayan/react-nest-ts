6ad197ccf5e203b07a9522d42368dee34d73b356
tarasov.d <d.tarasov@softmedialab.com>
2024-10-27 16:27:47 +0700

    add secure

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,6 +7,7 @@ async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule); // инициализируем экземпляр приложения, в create передаём модуль
   app.setGlobalPrefix('api')
+  app.enableCors()
   SwaggerConfigService.setupSwagger(app);
 
   // const jwtAuthGuard = app.get(JwtAuthGuard); // ограничение на уровне приложения
